b059abd336004402e1cd411c4a813af9dd492dc6
mochaza123 <58312722+mochaza123@users.noreply.github.com>
2021-03-21 18:34:20 +0700

    Add files via upload

---
+++ HappyBirthday.js
@@ -0,0 +1,27 @@
+var count = new Date("march 24, 2021 00:00:00").getTime();
+
+var x = setInterval(function() {
+
+    var now = new Date().getTime();
+
+    var distance = count - now;
+    
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+    
+        if (distance < 0) {
+
+        clearInterval(x);
+
+        window.location.replace("Masukkan Url Setelah Count Selesai");
+
+
+    }
+}, 1000);
---
+++ README.md
@@ -0,0 +1,21 @@
+# HBD
+Countdown Happy Birthday<br>
+Untuk Mengganti Tanggal Silahkan Buka File HappyBirthday.js<br>
+<ul>
+      <li>January</li>
+      <li>February</li>
+      <li>March</li>
+      <li>April</li>
+      <li>May</li>
+      <li>June</li>
+      <li>July</li>
+      <li>August</li>
+      <li>September</li>
+      <li>October</li>
+      <li>November</li>
+      <li>December</li>
+</ul>Sesuaikan bulan dengan list diatas
+      <p>Jangan Lupa Tambahkan URL yang akan dibuka bila countdown sudah timeout</p>
+if (distance < 0) {<br>
+window.location.replace("Masukkan Url Setelah Count Selesai");<br>
+}
